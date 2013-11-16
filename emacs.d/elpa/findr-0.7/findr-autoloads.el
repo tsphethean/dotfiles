d67554a124df54b6b83b62595f64886465df2ad0
Dan Mayer <dan.mayer@livingsocial.com>
2013-11-16 14:34:44 -0500

    kill submodule

--- emacs.d/elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19404 38711
-;;;;;;  704338))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; findr-autoloads.el ends here
